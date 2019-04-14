ad8926107e8edae417fd21650f3265bf569641d2
Marta Sušek <marta.susek@uniqcast.com>
2019-04-14 20:05:00 +0200

    api-gateway / add getPlaceInfo

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,47 +1,65 @@
-import { Controller, Get, Res, Query, Post, Logger, LoggerService } from '@nestjs/common';
+import { Controller, Get, Res, Query, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AxiosResponse } from 'axios';
 import { AutocompleteQuery } from './models/autocompleteQuery.model';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { ParseIntPipe } from './pipes/parseInt.pipe';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get('/api/profile_autocomplete')
-  async getProfiles(@Query() query: AutocompleteQuery, @Res() res): Promise<AxiosResponse<any[]>> {
+  async getProfiles(@Query() query: AutocompleteQuery, @Res() res: Response) {
     let currentValues;
     try {
       currentValues = this.validateCurrentValues(query.currentValues);
 
     } catch (err) {
-      res.status(500);
-      res.send(err.message);
+      res.status(400);
+      res.json(err.message);
       return;
     }
     delete query.currentValues;
     try {
       const r = await this.appService.getProfiles(query, currentValues);
-      return res.json(r);
+      res.json(r);
     } catch (err) {
-      res.status(500);
-      res.send(err.message);
+      res.status(400).json(err.message);
     }
-
   }
-  @Get()
-  root(@Res() res) {
+  @Get('/api/place_info/:id')
+  async getPlaceInfo(@Param('id', new ParseIntPipe()) id, @Res() res: Response): Promise<any> {
+    try {
+      const r = await this.appService.getPlaceInfo(id);
+      res.status(200);
+      res.json(r);
+    } catch (err) {
+      res.status(400);
+      res.json(err);
+    }
+  }
+
+  @Get('/')
+  root(@Res() res: Response) {
+    res.status(200);
     res.sendFile('index.html');
+
   }
 
-  validateCurrentValues = (currentValues: any) => {
+  validateCurrentValues(currentValues: string): number[] {
     if (currentValues.length === 0) {
-      return currentValues;
+      return null;
     }
-    const arr = currentValues.split(',');
-    const nums = [];
-    arr.forEach(element => {
+    let arr;
+    try {
+      arr = currentValues.split(',');
 
+    } catch (err) {
+      throw new Error('Validation error!');
+    }
+    const nums: number[] = [];
+
+    arr.forEach(element => {
       const val = parseInt(element, 10);
       if (!isNaN(val)) {
         nums.push(val);
--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -4,9 +4,9 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly httpService: HttpService) {}
-  async getProfiles(query: any, currentValues: number[]): Promise<AxiosResponse<any[]>> {
-    return  this.httpService.get('http://127.0.0.1:3001/api/profile_autocomplete', {
+  constructor(private readonly httpService: HttpService) { }
+  async getProfiles(query: any, currentValues: number[]) {
+    return this.httpService.get('http://127.0.0.1:3001/api/profile_autocomplete', {
       params: {
         query: query.query,
         type: query.type,
@@ -15,7 +15,15 @@ export class AppService {
       headers: {
         'Content-Type': 'application/json',
       },
-    }).toPromise().then(r => r.data).catch(err => {Logger.error(err.message, err.stack, 'ApiService', true); throw err; });
+    }).toPromise().then(r => r.data).catch(err => { Logger.error(err.message, err.stack, 'ApiService', true); throw err; });
+
+  }
+  async getPlaceInfo(id: number) {
+    return this.httpService.get(`http://127.0.0.1:3001/api/place_info/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).toPromise().then(r => r.data).catch(err => { Logger.error(err.message, err.stack, 'ApiService', true); throw err; });
 
   }
 
