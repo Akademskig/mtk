39354bfb98be8d589ee1bc154c2b50bcacbfb9a6
Marta Sušek <marta.susek@uniqcast.com>
2019-04-14 22:09:07 +0200

    api gateway / update test for controller

--- api-gateway/src/app.controller.spec.ts
+++ api-gateway/src/app.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpService, HttpModule, Res, Logger } from '@nestjs/common';
 import { AutocompleteQuery } from './models/autocompleteQuery.model';
 import { Places, PlaceItem, PlaceInfo } from './models/places.model';
 
@@ -9,15 +8,14 @@ jest.mock('./app.service');
 describe('AppController', () => {
   let appController: AppController;
   let appService: AppService;
-  const httpService = new HttpService();
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      imports: [HttpModule],
+      imports: [],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
-    appService = new AppService(httpService);
-    appController = new AppController(appService);
+    appService = app.get(AppService);
+    appController = app.get(AppController);
 
   });
 
@@ -25,7 +23,7 @@ describe('AppController', () => {
     it('should return a list of places', async () => {
       const query = new AutocompleteQuery('facebook', 'que', '1,2,3');
       const res = new Places([new PlaceItem(1, 'praćka')]);
-      jest.spyOn(appService, 'getProfiles').mockResolvedValue(Promise.resolve(res));
+      jest.spyOn(appService, 'getPlaces').mockResolvedValue(Promise.resolve(res));
       const result = await appController.getProfiles(query);
       expect(result).toMatchObject(expect.objectContaining({
         data: [{
@@ -33,6 +31,15 @@ describe('AppController', () => {
           name: expect.any(String),
         }],
       }));
+    });
+    it('should return an array of numbers', () => {
+      const query = new AutocompleteQuery('facebook', 'que', '1,2,3');
+      const result = appController.validateCurrentValues(query.currentValues);
+      expect(result).toEqual(expect.arrayContaining([1, 2, 3]));
+    });
+    it('should throw an error on invalid query', () => {
+      const query = new AutocompleteQuery('facebook', 'que', 'd,2,3');
+      expect(() => { appController.validateCurrentValues(query.currentValues); }).toThrow('Validation error!');
     });
   });
   describe('getPlaceInfo', () => {
@@ -45,16 +52,4 @@ describe('AppController', () => {
       ));
     });
   });
-  describe('validate an array of numbers', () => {
-    it('should return an array of numbers', () => {
-      const query = new AutocompleteQuery('facebook', 'que', '1,2,3');
-      const result = appController.validateCurrentValues(query.currentValues);
-      expect(result).toEqual(expect.arrayContaining([1, 2, 3]));
-    });
-    it('should throw an error on invalid query', () => {
-      const query = new AutocompleteQuery('facebook', 'que', 'd,2,3');
-      expect(() => { appController.validateCurrentValues(query.currentValues); }).toThrow('Validation error!');
-    });
-  });
-
 });
